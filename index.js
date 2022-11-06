774f53f43f43ce241131dc81b379e9804f1985ae
femas1 <federico.massara2@gmail.com>
2022-11-06 11:48:21 +0100

    implemented js for getting advice and putting in html

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
 
+const advice = document.querySelector('.container__advice');
+const adviceBtn = document.querySelector('.adviceButton');
+const slip_id = Math.trunc(Math.random() * 225);
+
 const giveMeAdvice = async function() {
     const res = await fetch(`https://api.adviceslip.com/advice/${slip_id}`);
     const data = await res.json();
-    console.log(data.slip.advice);
+    advice.innerText= data.slip.advice;
 }
 
-const slip_id = Math.trunc(Math.random() * 225);
-giveMeAdvice();
+adviceBtn.addEventListener('click', () => giveMeAdvice())
