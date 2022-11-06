0afc765781fb0aad896e41b51c3d9628174fb81a
femas1 <federico.massara2@gmail.com>
2022-11-06 14:35:50 +0100

    added js for getting advice id

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 
 const advice = document.querySelector('.container__advice');
 const adviceBtn = document.querySelector('.adviceButton');
+const adviceId = document.querySelector('#container__heading__id');
 
 const giveMeAdvice = async function() {
     let slip_id = Math.trunc(Math.random() * 225);
     const res = await fetch(`https://api.adviceslip.com/advice/${slip_id}`);
     const data = await res.json();
     advice.innerText= data.slip.advice;
+    adviceId.innerText = data.slip.id;
 }
 
 adviceBtn.addEventListener('click', () => giveMeAdvice())
