4a4af13408175b808e26f5ce0d3d47922aed27d3
femas1 <federico.massara2@gmail.com>
2022-11-06 11:39:05 +0100

    adjust number of rand ids possible

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict';
 
-// const giveMeAdvice = async function() {
-//     const res = await fetch(`https://api.adviceslip.com/advice/${slip_id}`);
-//     const data = await res.json();
-//     console.log(data);
-// }
+const giveMeAdvice = async function() {
+    const res = await fetch(`https://api.adviceslip.com/advice/${slip_id}`);
+    const data = await res.json();
+    console.log(data.slip.advice);
+}
 
-const slip_id = Math.trunc(Math.random() * 1000);
-console.log(slip_id);
-// giveMeAdvice();
+const slip_id = Math.trunc(Math.random() * 225);
+giveMeAdvice();
