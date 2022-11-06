e017336b1e77cdb393865e3ed1bf10394a72c00c
femas1 <federico.massara2@gmail.com>
2022-11-06 11:26:19 +0100

    set up basic api call

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 'use strict';
 
+const giveMeAdvice = async function() {
+    const res = await fetch(`https://api.adviceslip.com/advice`);
+    const data = await res.json();
+    console.log(data);
+}
+
+giveMeAdvice();
