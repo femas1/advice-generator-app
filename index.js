d62157fba1469b9e570dbcda552d4bee09c00ae8
femas1 <federico.massara2@gmail.com>
2022-11-06 14:09:56 +0100

    fixed function calling only once (moved random number into function)

--- index.js
+++ index.js
@@ -2,12 +2,11 @@
 
 const advice = document.querySelector('.container__advice');
 const adviceBtn = document.querySelector('.adviceButton');
-const slip_id = Math.trunc(Math.random() * 225);
 
 const giveMeAdvice = async function() {
+    let slip_id = Math.trunc(Math.random() * 225);
     const res = await fetch(`https://api.adviceslip.com/advice/${slip_id}`);
     const data = await res.json();
     advice.innerText= data.slip.advice;
+    console.log(data.slip.advice)
 }
-
-adviceBtn.addEventListener('click', () => giveMeAdvice())
