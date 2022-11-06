4525eee9f54569bb9d507b88bc9d204dcf64b746
femas1 <federico.massara2@gmail.com>
2022-11-06 11:36:22 +0100

    function for getting random id created

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const giveMeAdvice = async function() {
-    const res = await fetch(`https://api.adviceslip.com/advice`);
-    const data = await res.json();
-    console.log(data);
-}
+// const giveMeAdvice = async function() {
+//     const res = await fetch(`https://api.adviceslip.com/advice/${slip_id}`);
+//     const data = await res.json();
+//     console.log(data);
+// }
 
-giveMeAdvice();
+const slip_id = Math.trunc(Math.random() * 1000);
+console.log(slip_id);
+// giveMeAdvice();
